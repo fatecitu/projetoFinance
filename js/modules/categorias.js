de5e5789429fdaf75dd568cbb6242632d9327bc8
Ricardo Leeme <ricardo.leme@fatecitu.edu.br>
2025-10-28 09:17:08 -0300

    Aula 28/10 p1

--- js/modules/categorias.js
+++ js/modules/categorias.js
@@ -3,7 +3,7 @@ import { SUPABASE_URL, API_KEY } from "./config.js";
 //Buscar Lançamentos
 export async function buscarCategorias() {
     const token = localStorage.getItem('sb_token')
-    const res = await fetch(`${SUPABASE_URL}/rest/v1/categorias?select=*`, {
+    const res = await fetch(`${SUPABASE_URL}/rest/v1/categorias?select=*&order=tipo.asc,descricao.asc`, {
         headers: {
             "apikey": API_KEY,
             "Authorization": `Bearer ${token}`
--- js/ui/categorias.js
+++ js/ui/categorias.js
@@ -7,6 +7,11 @@ export async function carregarCategorias(){
     dados.forEach(categoria => {
         //criamos a linha com tr
         const tr = document.createElement('tr')
+        const tipoClasse = categoria.tipo === 'receita'
+              ? 'bg-green-200'
+              : 'bg-red-200'
+        tr.className = tipoClasse
+
         tr.innerHTML = `
         <td class='border border-gray-400 p-2'>${categoria.descricao}</td>
         <td class='border border-gray-400 p-2'>${categoria.tipo}</td>`
